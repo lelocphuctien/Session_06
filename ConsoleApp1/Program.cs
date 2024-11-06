0fd10acb269f13f196f012b3237bbe03a8f25e13
Administrator <Administrator@N301PC24>
2024-11-06 23:05:19 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Session_06;
+internal class Exercise_01
+{
+    public static void Main(string[] args)
+    {
+        Console.Write(" Enter the number: ");
+        int N = int.Parse(Console.ReadLine());
+        int[] array = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            Console.Write($"Enter value for item {i + 1}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("\nArray before incrementing: ");
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] += 2;
+        }
+        Console.WriteLine("\nArray before incrementing by 2: ");
+        foreach (int item in array)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
